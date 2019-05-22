8177c5991c9efeef7ce907a458c54b1350a2459b
Orbedi <orlandobedi@outlook.es>
2019-05-22 17:13:19 +0100

    Mensajes de error y tratamiento

--- src/main/java/com/crossfit/youtube/youtubeSOAPImpl.java
+++ src/main/java/com/crossfit/youtube/youtubeSOAPImpl.java
@@ -3,8 +3,8 @@ package com.crossfit.youtube;
 public class youtubeSOAPImpl implements Youtube {
 	
 	@Override
-	public String changeVideo(String url) {
-		if(!url.contains("www.youtube.com/watch?")) return "";
+	public String changeVideo(String url) throws QueryInvalidURL {
+		if(!url.contains("www.youtube.com/watch?")) throw new QueryInvalidURL("Invalid URL: ", url);
 		if(url.contains("&")) {
 			return url.substring(url.indexOf("v=")+2,url.indexOf("&"));
 		} else {
